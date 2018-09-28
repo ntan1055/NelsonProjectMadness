4eaf0fac8b6889af1c1d733d89a0db51a9de369a
BT_1N3_21@bktech.local <BT_1N3_21@bktech.local>
2018-09-28 08:41:25 -0400

    Initial commit

---
+++ src/NelsonTan/Main.java
@@ -0,0 +1,9 @@
+package NelsonTan;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("reverse of \'reverse\' is "+NelsonTanLib.reverse(input: "reverse"));
+    }
+}
---
+++ src/NelsonTan/NelsonTanLib.java
@@ -0,0 +1,15 @@
+package NelsonTan;
+
+public class NelsonTanLib {
+    public static String reverse(String input)
+    {
+        String output = "";
+        int i = input.length() - 1;
+        while (i >= 0)
+        {
+            output = output + input.substring(i, i + 1);
+            i--;
+        }
+        return output;
+    }
+}
